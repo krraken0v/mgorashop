b1901f5f3cedb5d82072acf87649bfda272e4b1c
last-commit <kohanickola@gmail.com>
2025-06-10 23:41:27 +0200

    all is cleared

--- server/server.js
+++ server/server.js
@@ -1,23 +1,25 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const newLocal = './routes/productRoutes';
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const newLocal = "./routes/productRoutes";
 const productRoutes = require(newLocal);
-const cors = require('cors');
-const morgan = require('morgan');
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
-const MONGO_URI = "mongodb+srv://mykola:bx48GZTaZI5J30O6@cluster0.1t6qrz9.mongodb.net/Mgorashop?retryWrites=true&w=majority&appName=Cluster0"
+const MONGO_URI = "";
 const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
-app.use('/products',productRoutes);
-mongoose.connect(MONGO_URI).then(console.log("MongoDB is Connected")).catch(err=>console.log(`MongoDBerror : ${err}`));
-app.get('/',(req,res)=>{
-    res.send('Mgorashop is running');
-
+app.use(morgan("dev"));
+app.use("/products", productRoutes);
+mongoose
+  .connect(MONGO_URI)
+  .then(console.log("MongoDB is Connected"))
+  .catch((err) => console.log(`MongoDBerror : ${err}`));
+app.get("/", (req, res) => {
+  res.send("Mgorashop is running");
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
